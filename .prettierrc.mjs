d1274b9ebe9753561f5941e6765f9667fd8275b9
Rinat Zaripov <zaripov.rinat@gmail.com>
2026-06-24 13:49:25 +1000

    style: trim prettier config header comments

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,9 +1,3 @@
-// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
-
-/**
- * @see https://prettier.io/docs/configuration
- * @type {import("prettier").Config}
- */
 export default {
   trailingComma: 'es5',
   tabWidth: 2,
